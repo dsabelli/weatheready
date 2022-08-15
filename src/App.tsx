3e0f506db3959ad6d4d9dc8388e10ec748c9be29
dsabelli <dsabelli3@gmail.com>
2022-08-15 16:48:18 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Weather Ready is still under development</h1>
+      <p className="read-the-docs">
+        Check out the{" "}
+        <a href="https://github.com/dsabelli/weatheready">github repo</a> to see
+        more!
+      </p>
+    </div>
+  );
+}
+
+export default App;
