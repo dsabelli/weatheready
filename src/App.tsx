c0e4166420d42c0accfb7c90ed509fcdb62f3b54
dsabelli <dsabelli3@gmail.com>
2022-08-23 08:35:45 -0400

    import and configure windicss

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Ready is still under development</h1>
-      <p className="read-the-docs">
+      <p className="read-the-docs ">
         Check out the{" "}
         <a href="https://github.com/dsabelli/weatheready">github repo</a> to see
         more!
