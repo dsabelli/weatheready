fd18218e35a18b48aae5cf587ea7eb2fd420d60b
dsabelli <dsabelli3@gmail.com>
2022-08-23 11:40:22 -0400

    set up weatherApiSlice with RTK Query

---
+++ src/features/api/weatherApiSlice.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const weatherApi = import.meta.env.VITE_WEATHER_API_KEY;
+
+export const weatherApiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `api.openweathermap.org/data/2.5`,
+  }),
+  tagTypes: ["Weather"],
+  endpoints: (builder) => ({
+    getWeather: builder.query({
+      query: () =>
+        `/forecast?lat=${42.0532}&lon=${82.5999}&appid=${weatherApi}`,
+      providesTags: ["Weather"],
+    }),
+  }),
+});
+
+export const { useGetWeatherQuery } = weatherApiSlice;
