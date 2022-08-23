181cf8769d716e6fa4b0ee638b44c454942ff346
dsabelli <dsabelli3@gmail.com>
2022-08-23 08:35:52 -0400

    import and configure windicss

---
+++ windi.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "windicss/helpers";
+import formsPlugin from "windicss/plugin/forms";
+
+export default defineConfig({
+  darkMode: "class",
+  attributify: true,
+  safelist: "p-3 p-4 p-5",
+  extract: {
+    include: ["src/**/*.{html,jsx,tsx}"],
+    exclude: ["node_modules", ".git"],
+  },
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          100: "#096",
+        },
+      },
+    },
+  },
+  plugins: [formsPlugin],
+});
