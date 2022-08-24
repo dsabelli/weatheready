0d09a1e587395e44edc6b6f8db9aa337805fdb69
dsabelli <dsabelli3@gmail.com>
2022-08-24 10:26:30 -0400

    import api slices into store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { weatherApiSlice } from "../features/api/weatherApiSlice";
+import { geoApiSlice } from "../features/api/geoApiSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [geoApiSlice.reducerPath]: geoApiSlice.reducer,
+    [weatherApiSlice.reducerPath]: weatherApiSlice.reducer,
+  },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware()
+  //     .prepend(geoApiSlice.middleware)
+  //     .concat(weatherApiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
