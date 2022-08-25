5f948f862f43311a45be39686d12933e598db263
dsabelli <dsabelli3@gmail.com>
2022-08-25 15:21:08 -0400

    switch windi to tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import "virtual:windi.css";
+import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
