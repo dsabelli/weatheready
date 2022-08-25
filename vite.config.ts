fc2e4c06160a3b67244657a1f2fc6061c5525b12
dsabelli <dsabelli3@gmail.com>
2022-08-25 15:20:51 -0400

    add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import WindiCSS from "vite-plugin-windicss";
-
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
+  plugins: [react(), pluginRewriteAll(), svgr()],
 });
