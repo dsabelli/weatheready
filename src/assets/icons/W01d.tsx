409e8c147d21cd3b7892f31352bee39ea7cfa49b
dsabelli <dsabelli3@gmail.com>
2022-08-25 20:05:43 -0400

    add assets

--- src/assets/icons/W01d.tsx
+++ src/assets/icons/W01d.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const W01d = () => {
-  return <div>W01d</div>;
+  return (
+    <div>
+      {" "}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+        <defs>
+          <linearGradient
+            id="a"
+            x1="26.75"
+            x2="37.25"
+            y1="22.91"
+            y2="41.09"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0" stopColor="#fbbf24" />
+            <stop offset=".45" stopColor="#fbbf24" />
+            <stop offset="1" stopColor="#f59e0b" />
+          </linearGradient>
+        </defs>
+        <circle
+          cx="32"
+          cy="32"
+          r="10.5"
+          fill="url(#a)"
+          stroke="#f8af18"
+          strokeMiterlimit="10"
+          strokeWidth=".5"
+        />
+        <path
+          fill="none"
+          stroke="#fbbf24"
+          stroke-linecap="round"
+          strokeMiterlimit="10"
+          strokeWidth="3"
+          d="M32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="45s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 32 32; 360 32 32"
+          />
+        </path>
+      </svg>
+    </div>
+  );
 };
 
 export default W01d;
--- src/assets/icons/W04d.tsx
+++ src/assets/icons/W04d.tsx
@@ -1,7 +1,42 @@
 import React from "react";
-
+// import "./W04d.css";
 const W04d = () => {
-  return <div>W04d</div>;
+  return (
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+        <defs>
+          {" "}
+          <linearGradient
+            id="a"
+            x1="22.56"
+            x2="39.2"
+            y1="21.96"
+            y2="50.8"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0" stopColor="#f3f7fe" />
+            <stop offset=".45" stopColor="#f3f7fe" />
+            <stop offset="1" stopColor="#deeafb" />
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#a)"
+          stroke="#e6effc"
+          strokeMiterlimit="10"
+          strokeWidth=".5"
+          d="M46.5 31.5h-.32a10.49 10.49 0 00-19.11-8 7 7 0 00-10.57 6 7.21 7.21 0 00.1 1.14A7.5 7.5 0 0018 45.5a4.19 4.19 0 00.5 0v0h28a7 7 0 000-14z"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="7s"
+            repeatCount="indefinite"
+            type="translate"
+            values="-3 0; 3 0; -3 0"
+          />
+        </path>
+      </svg>
+    </div>
+  );
 };
 
 export default W04d;
