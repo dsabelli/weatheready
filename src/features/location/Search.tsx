075143a966abb71c55c29eef0105ea8a19095c89
dsabelli <dsabelli3@gmail.com>
2022-08-25 20:06:07 -0400

    initial

---
+++ src/features/location/Search.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Search = () => {
+  // const {
+  //   data: geoData,
+  //   isLoading: isGeoLoading,
+  //   isSuccess: isGeoSuccess,
+  //   isError: isGeoError,
+  //   error: geoError,
+  // } = useGetGeoQuery(
+  //   {
+  //     city,
+  //     state,
+  //     country,
+  //   },
+  //   { refetchOnMountOrArgChange: true }
+  // );
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import CurrentWeather from "../features/weather/CurrentWeather";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <Link className="no-underline" to="today/a/a">
+        <CurrentWeather />
+      </Link>
+    </div>
+  );
 };
 
 export default Landing;
