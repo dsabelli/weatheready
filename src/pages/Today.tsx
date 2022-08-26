8e36f19bbcaa685643ccfbfb4c91e40c83018cdc
dsabelli <dsabelli3@gmail.com>
2022-08-26 14:34:40 -0400

    initial page setup

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ClothingCard from "../components/UI/ClothingCard";
+import TodayWeather from "../features/weather/TodayWeather";
 
 const Today = () => {
-  return <div>Today</div>;
+  return (
+    <div className="flex mx-auto justify-center max-w-4xl">
+      <TodayWeather />
+      <ClothingCard />
+    </div>
+  );
 };
 
 export default Today;
