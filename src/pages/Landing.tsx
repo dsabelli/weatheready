3033060384bda0df17b86aafb1c02e82663c3982
dsabelli <dsabelli3@gmail.com>
2022-08-26 09:48:33 -0400

    add placeholder

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ClothingCard from "../components/UI/ClothingCard";
+import NavBar from "../components/UI/NavBar";
 import CurrentWeather from "../features/weather/CurrentWeather";
 
 const Landing = () => {
   return (
     <div>
-      <Link className="no-underline" to="today/a/a">
-        <CurrentWeather />
-      </Link>
+      <NavBar />
+      <div className="flex mx-auto justify-center max-w-4xl">
+        <Link className="" to="today/a/a">
+          <CurrentWeather />
+        </Link>
+        <ClothingCard />
+      </div>
     </div>
   );
 };
