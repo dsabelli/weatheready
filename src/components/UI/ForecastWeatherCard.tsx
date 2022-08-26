da507b7f157776492e160a2779bd99628c67d588
dsabelli <dsabelli3@gmail.com>
2022-08-26 14:34:01 -0400

    forecast weather

---
+++ src/components/UI/ForecastWeatherCard.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { getIcon } from "../../utils/getIcon";
+
+interface ForecastCardData {
+  temp: number;
+  feelsLike: number;
+  icon: string;
+  description: string;
+  name?: string;
+}
+let units: string = "°C";
+
+const ForecastWeatherCard: React.FC<ForecastCardData> = ({
+  temp,
+  feelsLike,
+  icon,
+  description,
+}) => {
+  return (
+    <div className="flex bg-base-100 shadow-xl p-4 mx-auto max-w-2xl">
+      <div className="flex flex-col w-full">
+        <div className="text-2xl ">
+          <h2>Forecast Weather</h2>
+        </div>
+        <div className="flex w-full">
+          <div className="flex justify-center items-center w-full">
+            <div className="w-48 ">{getIcon(icon)}</div>
+            <div>
+              <p className="text-4xl">
+                {Math.round(temp)}
+                {units}
+              </p>
+              <p className="text-sm">
+                Feels like{" "}
+                <span className="text-xl">
+                  {Math.round(feelsLike)}
+                  {units}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center w-full">
+            {description}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForecastWeatherCard;
---
+++ src/features/weather/TodayWeather.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import ForecastWeatherCard from "../../components/UI/ForecastWeatherCard";
+import Loader from "../../components/UI/Loader";
+import { useGetForecastQuery } from "../../features/weatherApi/weatherApiSlice";
+import Error from "../../pages/Error";
+const TodayWeather = () => {
+  const { lat, lon } = useSelector((state: RootState) => state.location);
+  const {
+    data: weatherData,
+    isLoading: isWeatherLoading,
+    isSuccess: isWeatherSuccess,
+    isError: isWeatherError,
+    error: weatherError,
+  } = useGetForecastQuery({
+    lat: lat,
+    lon: lon,
+  });
+  console.log(weatherData);
+
+  let weatherEls;
+
+  if (isWeatherLoading) {
+    weatherEls = <Loader />;
+  } else if (isWeatherSuccess) {
+    console.log(weatherData);
+
+    weatherEls = (
+      <ForecastWeatherCard
+        temp={22}
+        feelsLike={24}
+        icon="01d"
+        description="sunny"
+        name="Leamington"
+      />
+    );
+  } else if (isWeatherError) {
+    weatherEls = <Error />;
+  }
+
+  return <div>{weatherEls}</div>;
+};
+
+export default TodayWeather;
