508710f92a31693bc577fa2db301c3cdb4639a10
dsabelli <dsabelli3@gmail.com>
2022-08-29 09:14:19 -0400

    add new types for OneCall API

--- src/types.ts
+++ src/types.ts
@@ -104,3 +104,56 @@ export interface ForecastData {
     };
   };
 }
+
+export interface DailyWeather {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  rain: number;
+  snow: number;
+  uvi: number;
+}
+
+export interface Alerts {
+  sender_name: string;
+  event: string;
+  start: number;
+  end: number;
+  description: string;
+}
+
+export interface OneCallData {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  hourly: ListWeather[];
+  daily: DailyWeather[];
+  alerts: Alerts[];
+}
