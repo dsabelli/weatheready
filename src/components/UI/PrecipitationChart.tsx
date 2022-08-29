57cf016e2d856e4ae87fff1eb898c02d96a12cae
dsabelli <dsabelli3@gmail.com>
2022-08-29 13:26:36 -0400

    add chart for precipitation

---
+++ src/components/UI/PrecipitationChart.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Area,
+  ResponsiveContainer,
+} from "recharts";
+
+interface PrecipitationData {
+  data: MinuteData[];
+}
+
+interface MinuteData {
+  dt: number;
+  precipitation: number;
+}
+
+const PrecipitationChart: React.FC<PrecipitationData> = ({ data }) => {
+  const precipData = data.map((d) => {
+    const hours = new Date(d.dt * 1000).getHours();
+    const minutes = new Date(d.dt * 1000).getMinutes();
+    return { ...d, dt: `${hours}:${minutes < 10 ? 0 : ""}${minutes}` };
+  });
+
+  const hidePrecipData = precipData.every((d) => d.precipitation === 0);
+  return (
+    <div
+      className={` bg-base-100 shadow-xl p-4 mx-auto ${
+        hidePrecipData ? "hidden" : ""
+      }`}
+    >
+      {!hidePrecipData && (
+        <ResponsiveContainer aspect={4}>
+          <AreaChart
+            width={600}
+            height={100}
+            data={precipData}
+            margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+          >
+            <XAxis dataKey="dt" />
+            <YAxis dataKey="precipitation" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="precipitation"
+              stroke="#387908"
+              fill="#387908"
+              yAxisId={0}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      )}
+    </div>
+  );
+};
+
+export default PrecipitationChart;
