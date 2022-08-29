d7fb2901b067413b99babc1446fb0b806639d00e
dsabelli <dsabelli3@gmail.com>
2022-08-29 17:32:29 -0400

    pass weatherdata to forecastweathercard

--- src/features/weather/TodayWeather.tsx
+++ src/features/weather/TodayWeather.tsx
@@ -5,6 +5,7 @@ import ForecastWeatherCard from "../../components/UI/ForecastWeatherCard";
 import Loader from "../../components/UI/Loader";
 import { useGetOneCallQuery } from "../../features/weatherApi/weatherApiSlice";
 import Error from "../../pages/Error";
+import { nanoid } from "nanoid";
 const TodayWeather = () => {
   const { lat, lon } = useSelector((state: RootState) => state.location);
   const {
@@ -28,11 +29,24 @@ const TodayWeather = () => {
 
     weatherEls = (
       <ForecastWeatherCard
-        temp={22}
-        feelsLike={24}
-        icon="01d"
-        description="sunny"
-        name="Leamington"
+        key={nanoid()}
+        todayTemp={weatherData.daily[0].temp}
+        todayFeelsLike={weatherData.daily[0].feels_like}
+        icon={weatherData.daily[0].weather[0].icon}
+        description={weatherData.daily[0].weather[0].description}
+        iconWidth={`w-24`}
+        title={`title???`}
+        date={weatherData.daily[0].dt}
+        rain={weatherData.daily[0].rain}
+        snow={weatherData.daily[0].snow}
+        pop={weatherData.daily[0].pop}
+        windSpeed={weatherData.daily[0].wind_speed}
+        windGust={weatherData.daily[0].wind_gust}
+        uvi={weatherData.daily[0].uvi}
+        humidity={weatherData.daily[0].humidity}
+        clouds={weatherData.daily[0].clouds}
+        temp={0}
+        feelsLike={0}
       />
     );
   } else if (isWeatherError) {
