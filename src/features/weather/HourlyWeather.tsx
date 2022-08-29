da9bff3db3261ef4067547117c783e13175628c4
dsabelli <dsabelli3@gmail.com>
2022-08-29 17:32:22 -0400

    pass weatherdata to forecastweathercard

---
+++ src/features/weather/HourlyWeather.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import ForecastWeatherCard from "../../components/UI/ForecastWeatherCard";
+import Loader from "../../components/UI/Loader";
+import { useGetOneCallQuery } from "../../features/weatherApi/weatherApiSlice";
+import Error from "../../pages/Error";
+import { nanoid } from "nanoid";
+const HourlyWeather = () => {
+  const { lat, lon } = useSelector((state: RootState) => state.location);
+  const {
+    data: weatherData,
+    isLoading: isWeatherLoading,
+    isSuccess: isWeatherSuccess,
+    isError: isWeatherError,
+    error: weatherError,
+  } = useGetOneCallQuery({
+    lat: lat,
+    lon: lon,
+  });
+  console.log(weatherData);
+
+  let weatherEls;
+
+  if (isWeatherLoading) {
+    weatherEls = <Loader />;
+  } else if (isWeatherSuccess) {
+    console.log(weatherData);
+
+    weatherEls = weatherData.hourly
+      .filter(
+        (day) => new Date(day.dt * 1000).getDate() === new Date().getDate()
+      )
+      .map((hour) => (
+        <ForecastWeatherCard
+          key={nanoid()}
+          temp={hour.temp}
+          feelsLike={hour.feels_like}
+          icon={hour.weather[0].icon}
+          description={hour.weather[0].description}
+          iconWidth={`w-24`}
+          date={hour.dt}
+          rain={0}
+          snow={0}
+          pop={hour.pop || 0}
+          windSpeed={hour.wind_speed}
+          windGust={hour.wind_gust}
+          uvi={hour.uvi}
+          humidity={hour.humidity}
+          clouds={hour.clouds}
+        />
+      ));
+  } else if (isWeatherError) {
+    weatherEls = <Error />;
+  }
+
+  return <div>{weatherEls}</div>;
+};
+
+export default HourlyWeather;
