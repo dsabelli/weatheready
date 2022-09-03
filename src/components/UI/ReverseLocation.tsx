c84a381be1269628fe694302edf4e93bc494e04b
dsabelli <dsabelli3@gmail.com>
2022-09-03 18:15:09 -0400

    add weather data to navbar

--- src/components/UI/ReverseLocation.tsx
+++ src/components/UI/ReverseLocation.tsx
@@ -3,9 +3,30 @@ import { Loader } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { useGetReverseLocationQuery } from "../../features/reverseLocationApi/reverseLocationSlice";
+import { useGetOneCallQuery } from "../../features/weatherApi/weatherApiSlice";
 
 const ReverseLocation = () => {
   const location = useSelector((state: RootState) => state.location);
+  const { metric } = useSelector((state: RootState) => state.settings);
+
+  const {
+    data: weatherData,
+    isLoading: isWeatherLoading,
+    isSuccess: isWeatherSuccess,
+    isError: isWeatherError,
+    error: weatherError,
+  } = useGetOneCallQuery({
+    lat: location.lat,
+    lon: location.lon,
+    units: metric ? "metric" : "imperial",
+  });
+
+  let weatherEls;
+
+  if (isWeatherSuccess) {
+    const current = weatherData.current;
+    weatherEls = <div>{current.temp}</div>;
+  }
   const {
     data: reverseLocationData,
     isLoading: isReverseLocationLoading,
@@ -19,14 +40,17 @@ const ReverseLocation = () => {
 
   let reverseLocationEl;
 
-  if (isReverseLocationLoading) {
-    reverseLocationEl = <Loader />;
-  } else if (isReverseLocationSuccess) {
+  if (isReverseLocationSuccess) {
     reverseLocationEl = (
       <div>{reverseLocationData.features[0].properties.city}</div>
     );
   }
-  return <div>{reverseLocationEl}</div>;
+  return (
+    <div>
+      {reverseLocationEl}
+      {weatherEls}
+    </div>
+  );
 };
 
 export default ReverseLocation;
