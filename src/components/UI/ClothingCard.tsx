de4924207e471bb695467cb1f14dc8535ddea249
dsabelli <dsabelli3@gmail.com>
2022-09-06 14:23:50 -0400

    set up clothing recommendations

--- src/components/UI/ClothingCard.tsx
+++ src/components/UI/ClothingCard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const ClothingCard = ({}) => {
+const ClothingCard = ({ clothing }) => {
   return (
     <div className="flex bg-base-200 shadow-xl p-4  ">
       <div className="text-xl ">
-        <h2>Clothing Recommendation</h2>
+        <h2>
+          {clothing.top}
+          {clothing.bottom}
+        </h2>
       </div>
     </div>
   );
