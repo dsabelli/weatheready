5476f9febbf004f70dfb4267d91f0d754c386236
dsabelli <dsabelli3@gmail.com>
2022-09-12 16:48:03 -0400

    set all text colour

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = () => {
   let location = useLocation();
   const pathname = location.pathname;
   return (
-    <div className="flex flex-col justify-between h-screen">
+    <div className="flex flex-col justify-between h-screen text-primary-content">
       <div className="w-full">
         <NavBar />
         <AlertBanner />
