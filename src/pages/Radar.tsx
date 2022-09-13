cd71adf7345c2552d68d6eca5a174fd37184734f
dsabelli <dsabelli3@gmail.com>
2022-09-13 11:19:03 -0400

    padding right for mobile scroll room

--- src/pages/Radar.tsx
+++ src/pages/Radar.tsx
@@ -3,7 +3,7 @@ import Map from "../features/weatherRadarApi/Map";
 
 const Radar = () => {
   return (
-    <div>
+    <div className="pr-6 md:pr-0">
       <Map height={"70vh"} />
     </div>
   );
