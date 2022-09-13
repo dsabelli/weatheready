2cae96168a9133e79a650adede28121413195b6b
dsabelli <dsabelli3@gmail.com>
2022-09-13 09:58:37 -0400

    save checked state

--- src/components/UI/ThemeToggle.tsx
+++ src/components/UI/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { setTheme } from "../../features/settings/settingsSlice";
@@ -6,17 +6,20 @@ import { setTheme } from "../../features/settings/settingsSlice";
 const ThemeToggle = () => {
   const dispatch = useDispatch();
   const { theme } = useSelector((state: RootState) => state.settings);
+  const [checked, setChecked] = useState(theme === "light" ? false : true);
 
   return (
     <div className="form-control">
       <label className="label cursor-pointer gap-1">
         <p>Light</p>
         <input
+          checked={checked}
           className="toggle"
           type="checkbox"
-          onChange={() =>
-            dispatch(setTheme(theme === "light" ? "dark" : "light"))
-          }
+          onChange={() => (
+            dispatch(setTheme(theme === "light" ? "dark" : "light")),
+            setChecked((prev) => !prev)
+          )}
         />
         <p>Dark</p>
       </label>
--- src/components/UI/UnitsToggle.tsx
+++ src/components/UI/UnitsToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { setUnits } from "../../features/settings/settingsSlice";
@@ -6,15 +6,19 @@ import { setUnits } from "../../features/settings/settingsSlice";
 const UnitsToggle = () => {
   const dispatch = useDispatch();
   const { metric } = useSelector((state: RootState) => state.settings);
+  const [checked, setChecked] = useState(metric ? false : true);
 
   return (
     <div className="form-control">
       <label className="label cursor-pointer gap-1">
         <p>°C</p>
         <input
+          checked={checked}
           className="toggle"
           type="checkbox"
-          onChange={() => dispatch(setUnits(!metric))}
+          onChange={() => (
+            dispatch(setUnits(!metric)), setChecked((prev) => !prev)
+          )}
         />
         <p>°F</p>
       </label>
