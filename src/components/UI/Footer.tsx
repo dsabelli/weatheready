50d766cda813f2620f29ec3fae6142c91f0e4cef
dsabelli <dsabelli3@gmail.com>
2022-09-13 09:01:49 -0400

    add toggle slider

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import SettingsToggle from "./SettingsToggle";
 
 const Footer = () => {
   return (
     <footer
       className={`footer footer-center mt-4 p-4 bg-base-300 text-base-content`}
     >
-      <div>
+      <div className="flex w-full justify-between">
+        <SettingsToggle />
         <p>Copyright © {new Date().getFullYear()} weatheready.com</p>
       </div>
     </footer>
