14d2dc379b2b3d2b2ced34a9da563a5453033589
dsabelli <dsabelli3@gmail.com>
2022-10-25 10:29:31 -0400

    config for test server

--- cypress/e2e/weatheready.cy.ts
+++ cypress/e2e/weatheready.cy.ts
@@ -1,6 +1,12 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://weatheready.com");
+/// <reference types="cypress"/>
+
+describe("Landing Page Functionality", () => {
+  it("visits weatheready.com", () => {
+    cy.visit("/");
+  });
+
+  it("shows tooltip on hover", () => {
+    cy.get(".feather-pause").click();
   });
 });
 
