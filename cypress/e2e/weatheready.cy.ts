da17c176628031139e77073c5bba54ff03489dcd
dsabelli <dsabelli3@gmail.com>
2022-10-31 08:52:30 -0400

    check barometer button disappears

--- cypress/e2e/weatheready.cy.ts
+++ cypress/e2e/weatheready.cy.ts
@@ -18,73 +18,77 @@ function mockLocation(latitude: number, longitude: number) {
   };
 }
 
-// describe("Landing Page Functionality", () => {
-//   it("visits weatheready.com", () => {
-//     cy.visit("/", mockLocation(42.0531, -82.5998));
-//   });
+describe("Landing Page Functionality", () => {
+  it("visits weatheready.com", () => {
+    cy.visit("/", mockLocation(42.0531, -82.5998));
+  });
 
-//   it("toggles units from C to F", () => {
-//     cy.get(".units-toggle").click();
-//     cy.contains("mph");
-//   });
+  it("toggles units from C to F", () => {
+    cy.get(".units-toggle").click();
+    cy.contains("mph");
+  });
 
-//   it("toggles units from F to C", () => {
-//     cy.get(".units-toggle").click();
-//     cy.contains("km/h");
-//   });
+  it("toggles units from F to C", () => {
+    cy.get(".units-toggle").click();
+    cy.contains("km/h");
+  });
 
-//   it("toggles units from light to dark", () => {
-//     cy.get(".theme-toggle").click();
-//     cy.get("[data-theme='dark']");
-//   });
+  it("toggles units from light to dark", () => {
+    cy.get(".theme-toggle").click();
+    cy.get("[data-theme='dark']");
+  });
 
-//   it("toggles units from dark to light", () => {
-//     cy.get(".theme-toggle").click();
-//     cy.get("[data-theme='light']");
-//   });
+  it("toggles units from dark to light", () => {
+    cy.get(".theme-toggle").click();
+    cy.get("[data-theme='light']");
+  });
 
-//   it("shows precip chart if raining", () => {
-//     if (cy.contains("Rain")) {
-//       cy.get(".precip-chart");
-//     } else return;
-//   });
+  it("shows precip chart if raining", () => {
+    if (cy.contains("Rain")) {
+      cy.get(".precip-chart");
+    } else return;
+  });
 
-//   it("reveals modal on click, answers and submits quiz", () => {
-//     cy.get(".modal-button").click();
-//     cy.get("[data-cy='r0']").click();
-//     cy.get("[data-cy='submit-quiz']").click();
-//   });
+  it("reveals modal on click, answers and submits quiz", () => {
+    cy.get(".modal-button").click();
+    cy.get("[data-cy='r0']").click();
+    cy.get("[data-cy='submit-quiz']").click();
+  });
 
-//   it("toggles radar on and off", () => {
-//     let time = cy.get(".radar-time");
-//     cy.get(".feather-pause").click();
-//     cy.wait(800);
-//     cy.get(".feather-play").click();
-//     cy.wrap(".radar-time").should("not.equal", time);
-//   });
+  it("modal hidden after completing quiz", () => {
+    cy.get(".modal-button").should("not.exist");
+  });
 
-//   it("searches for a new location and stores as recent", () => {
-//     cy.get("input.search-input").type("London");
-//     cy.contains("London, England").click();
-//     cy.contains("London, England");
-//     cy.get("input.search-input").focus();
-//     cy.get(".opacity-60").contains("London, England");
-//   });
+  it("toggles radar on and off", () => {
+    let time = cy.get(".radar-time");
+    cy.get(".feather-pause").click();
+    cy.wait(800);
+    cy.get(".feather-play").click();
+    cy.wrap(".radar-time").should("not.equal", time);
+  });
 
-//   // it("speeds up radar", () => {
-//   //   cy.get(".feather-pause").click();
-//   //   cy.wait(3200);
-//   //   cy.get(".feather-play").click();
-//   //   let time1 = cy.get(".radar-time");
-//   //   //set back to original position
-//   //   cy.get(".feather-pause").click();
-//   //   cy.wait(1400);
-//   //   cy.get(".feather-play").click();
-//   //   //   let thumb = cy.get(".speed-control");
-//   //   //   thumb.trigger("mousedown");
-//   //   //   thumb.trigger("mousemove");
-//   // });
-// });
+  it("searches for a new location and stores as recent", () => {
+    cy.get("input.search-input").type("London");
+    cy.contains("London, England").click();
+    cy.contains("London, England");
+    cy.get("input.search-input").focus();
+    cy.get(".opacity-60").contains("London, England");
+  });
+
+  // it("speeds up radar", () => {
+  //   cy.get(".feather-pause").click();
+  //   cy.wait(3200);
+  //   cy.get(".feather-play").click();
+  //   let time1 = cy.get(".radar-time");
+  //   //set back to original position
+  //   cy.get(".feather-pause").click();
+  //   cy.wait(1400);
+  //   cy.get(".feather-play").click();
+  //   //   let thumb = cy.get(".speed-control");
+  //   //   thumb.trigger("mousedown");
+  //   //   thumb.trigger("mousemove");
+  // });
+});
 
 describe("Routing Functionality", () => {
   it("visits weatheready.com", () => {
